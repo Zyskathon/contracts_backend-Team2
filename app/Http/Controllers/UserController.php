a9fda7056044df61a17d4d5795728eb0b3dc0f52
Ganapati <ganapati.manjunath@zysk.tech>
2023-10-06 12:30:09 +0530

    changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    // public function
+    public function login(Request $request)
+    {
+        $data = Request::create('/oauth/token', 'POST', $request->all());
+        $response = app()->handle($data);
+        if (!$response->isSuccessful()) {
+            return response('Invalid credentials', 401);
+        }
+        return $response; //what if the user is not there?
+    }
 }
